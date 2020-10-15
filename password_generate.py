fea92f6977b1035fe6d988c69fe743bdc1c01d40
Abdullah Aish <abdullahaish7@gmail.com>
2020-10-15 23:50:42 +0500

    Password Generator

--- password_generate.py
+++ password_generate.py
@@ -1,22 +1,17 @@
+# Python program to generate random 
+# password using Tkinter module 
+import random 
+from tkinter import messagebox
 from tkinter import *
-from tkinter import ttk
-import random
+from tkinter.ttk import *
+import pyperclip
 
-root = Tk()
+# Function for calculation of password 
+def low(): 
+	# entry.delete(0, END) 
 
-# Title of your GUI window 
-root.title("Random Password Generator") 
-
-# Size of you window
-root.geometry("500x150")
-
-# Icon of your window
-root.iconbitmap("pass.ico")
-
-# Create Function
-def low():  
 	# Get the length of passowrd 
-	length = mycombo.get() 
+	length = var1.get() 
 
 	lower = "abcdefghijklmnopqrstuvwxyz"
 	upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -24,54 +19,87 @@ def low():
 	password = "" 
 
 	# if strength selected is low 
-	if length == "Low": 
+	if var.get() == 1: 
 		for i in range(0, length): 
 			password = password + random.choice(lower) 
-		return password  
+		return password 
 
 	# if strength selected is medium 
-	if length == "Medium": 
+	elif var.get() == 0: 
 		for i in range(0, length): 
 			password = password + random.choice(upper) 
 		return password 
 
 	# if strength selected is strong 
-	if length == "High": 
+	elif var.get() == 3: 
 		for i in range(0, length): 
 			password = password + random.choice(digits) 
-		return password  
+		return password 
+	else: 
+		print("Please choose an option") 
 
-def generate():
-    entry.delete(0, END)
-    password = low()
-    entry.insert(0, password)
 
-Random_password = Label(root, text="Password: ") 
-Random_password.grid(row=2, column=0) 
-# entry = Entry(root) 
-# entry.grid(row=2, column=1)
+# Function for generation of password 
+def generate(): 
+	global entry
+	if combo.get() == "" or combo.get() == "Length":
+		messagebox.showwarning("Warning", "Please Enter Length!")
+		# messagebox.showinfo("Show Info", "Message Box")
+	else:
+		password1 = low() 
+		Random_password = Label(root, text="Password: ") 
+		Random_password.grid(row=3) 
+		entry = Entry(root) 
+		entry.grid(row=3, column=1, pady=5) 
+		entry.insert(10, password1) 
+		messagebox.showinfo("Password Generator", "Password Generate Successfully!")
+		copy_button = Button(root, text="Copy", command=copy1) 
+		copy_button.grid(row=3, column=2, padx=5)
+	
+def copy1(): 
+	random_password = entry.get() 
+	pyperclip.copy(random_password) 
 
-length = Label(root, text="Length: ", pady=10).grid(row=0)
-# Password Types Low Medium Strong
-pass_type = Label(root, text="Type: ").grid(row=1)
 
-# Create Generate Button
-generate_Button = Button(root, text="Generate", command=generate).grid(row=3, column=1, padx=5)
-# btn = Button(root, text="btn", command= abc).grid(row=3,column=1)
+# Main Function 
 
-# Create Combo box
-options = (8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, "Length")
-type_pas = ["Low" , "Medium", "High"]
-mycombo = ttk.Combobox(root, value=options)
-mycombo.current(0)
-mycombo.bind("<<ComboboxSelected>>")
-mycombo.set("Select")
-mycombo.grid(row=0, column=1)
+# create GUI window 
+root = Tk() 
+root.iconbitmap("pass.ico")
+root.geometry("300x120")
+var = IntVar() 
+var1 = IntVar() 
 
-var = ttk.Combobox(root, value=type_pas)
-var.current(0)
-var.bind("<<ComboboxSelected>>")
-var.set("Select")
-var.grid(row=1, column=1)
+# Title of your GUI window 
+root.title("Password Generator") 
 
-root.mainloop()
+# create label for length of password 
+c_label = Label(root, text="Length: ") 
+c_label.grid(row=0, pady=5) 
+
+
+generate_button = Button(root, text="Generate", command=generate) 
+generate_button.grid(row=2, column=1, pady=5)  
+
+# Radio Buttons for deciding the 
+# strength of password 
+# Default strength is Medium 
+radio_low = Radiobutton(root, text="Low", variable=var, value=1) 
+radio_low.grid(row=1, column=0) 
+radio_middle = Radiobutton(root, text="Medium", variable=var, value=0) 
+radio_middle.grid(row=1, column=1) 
+radio_strong = Radiobutton(root, text="Strong", variable=var, value=3) 
+radio_strong.grid(row=1, column=2) 
+combo = Combobox(root, textvariable=var1) 
+
+# Combo Box for length of your password 
+combo['values'] = (8, 9, 10, 11, 12, 13, 14, 15, 16, 
+				17, 18, 19, 20, 21, 22, 23, 24, 25, 
+				26, 27, 28, 29, 30, 31, 32, "Length") 
+combo.current(0) 
+combo.bind('<<ComboboxSelected>>') 
+combo.set("")
+combo.grid(column=1, row=0) 
+
+# start the GUI 
+root.mainloop() 
